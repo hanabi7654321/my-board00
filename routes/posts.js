fe18f708b09a87d96077064f64a0675166b86dd8
Hanabi <hanabi1a21a2@gmail.com>
2021-01-25 02:50:46 +0900

    Designed the top page

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,19 @@
-'use strict';
 const express = require('express');
 const router = express.Router();
 
 /* GET posts page. */
 router.get('/', (req, res, next) => {
-  res.send('posts Page!');
+  res.render('posts');
 });
 
+// router.get('/posts', (req, res) => {
+//   const name = req.param("name")
+//   const body = req.param("body")
+//   if ( !name || !body ) {
+//     res.send('名前もしくは本文を入力してください')
+//     return
+//   }
+//   res.redirect("/")
+// })
 
 module.exports = router;
